0f74fe06ffbf2499a45629ef31b1708ab8c9261b
gcokuu <alexamarie256@gmail.com>
2023-03-11 00:37:59 -0800

    item interaction / environment

--- DGD III Project/Assets/Scripts/ItemInteraction.cs
+++ DGD III Project/Assets/Scripts/ItemInteraction.cs
@@ -3,17 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-
-
 public class ItemInteraction : MonoBehaviour
 {
 
-    public TextMeshPro itemText;
+    public TextMeshProUGUI itemText;
     public GameObject textObj;
     // Start is called before the first frame update
     void Start()
     {
-        itemText = textObj.GetComponent<TextMeshPro>();
+        itemText = textObj.GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
@@ -22,17 +20,34 @@ public class ItemInteraction : MonoBehaviour
 
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnTriggerEnter(Collider collision)
     {
+      
         if (collision.gameObject.tag == "Food")
         {
+
+            itemText.text = "Press E to Eat";
             itemText.enabled = true;
+
+        } else if (collision.gameObject.tag == "Health")
+        {
+
+            itemText.text = "Press E to Use";
+            itemText.enabled = true;
+
+        } else if (collision.gameObject.tag == "Plank")
+        {
+
+            itemText.text = "Press E to Collect";
+            itemText.enabled = true;
+
         }
     }
 
-    void OnCollisionExit(Collision collision)
+    void OnTriggerExit(Collider collision)
     {
-        if (collision.gameObject.tag == "Food")
+       
+        if (collision.gameObject.tag == "Food" || collision.gameObject.tag == "Health" || collision.gameObject.tag == "Plank")
         {
             itemText.enabled = false;
         }

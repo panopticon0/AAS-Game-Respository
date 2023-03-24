27790220391a3d49acc975239aa9c39e97026ea2
panopticon0 <113198665+panopticon0@users.noreply.github.com>
2023-03-24 09:06:27 -0700

    comments

--- DGD III Project/Assets/Scripts/enemySpawn.cs
+++ DGD III Project/Assets/Scripts/enemySpawn.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class enemySpawn : MonoBehaviour
 {
+    //radius that enemies can spawn from spawner object position
     private float radius = 15f;
+    //number of enemies that the object should spawn
     public int numEne = 5;
+    //types of enemies the spawner should spawn
     public List<GameObject> enemyType;
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < numEne; i++)
         {
+            //repeatedly spawn enemies in random positions within the range until i is equal to number of enemies that the object should spawn
             Instantiate(enemyType[(int)Random.Range(0, 2)], transform.position + new Vector3(Random.Range(-radius, radius), 0, Random.Range(-radius, radius)), transform.rotation);
             Debug.Log(i);
         }

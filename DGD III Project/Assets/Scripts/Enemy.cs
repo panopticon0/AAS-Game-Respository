88effa2d9caa73c2056d489763546afc8032f22e
panopticon0 <113198665+panopticon0@users.noreply.github.com>
2023-04-06 09:07:53 -0700

    Enemy obeys gravity
    You'll need to add a character controller component to the prefab, though. Tell me if you need assistance with it or adjusting the variables

--- DGD III Project/Assets/Scripts/Enemy.cs
+++ DGD III Project/Assets/Scripts/Enemy.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private float health = 5.0f;
-    public float speed = 2.5f;
+    private float health = 10.0f;
+    private float speed = 5f;
     private Transform player;
     public float range = 1.5f;
     private float deathTime = 1.0f;
@@ -13,31 +13,52 @@ public class Enemy : MonoBehaviour
     public Animator enemyAnim;
     private Vector3 startPos;
     public Transform head;
+    private CharacterController enemyAI;
+
+    public float gravityValue = 3f;
+    public float gravityMultiplier = 1.1f;
+    public float enemyYVelocity = 3f;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindWithTag("Player").transform;
         enemyAnim = GetComponent<Animator>();
         startPos = transform.position;
+        enemyAI = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 direction = player.position - transform.position;
+        Vector3 velocity = direction * speed;
+        float gravity = gravityValue * gravityMultiplier * Time.deltaTime;
         float distance = Vector3.Distance(player.position, transform.position);
+        if (enemyAI.isGrounded)
+        {
+                
+        } else
+        {
+            enemyYVelocity -= gravity;
+        }
+        velocity.y = enemyYVelocity;
+        velocity.Normalize();
         if (distance <= range)
         {
             transform.LookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z));
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
             enemyAnim.SetBool("playerDetected", true);
+            enemyAI.Move(speed * velocity * Time.deltaTime);
         } else
         {
             enemyAnim.SetBool("playerDetected", false);
             if (Vector3.Distance(startPos, transform.position) >= 2)
             {
+                direction = startPos - transform.position;
+                velocity = direction * speed;
                 enemyAnim.SetBool("playerDetected", true);
                 transform.LookAt(new Vector3(startPos.x, transform.position.y, startPos.z));
-                transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                velocity.Normalize();
+                enemyAI.Move(speed * velocity * Time.deltaTime);
             }
         }
         if (health <= 0)
@@ -66,26 +87,22 @@ public class Enemy : MonoBehaviour
             Destroy(other.gameObject);
             Debug.Log(health);
         }
-
     }
 
-    void OnCollisionEnter(Collision other)
+    void OnCollisionEnter(Collision collision)
     {
-        if (other.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
             Debug.Log("Attacking!");
             enemyAnim.SetBool("attacking", true);
         }
     }
 
-    void OnCollisionExit(Collision other)
+    void OnCollisionExit(Collision collision2)
     {
-        if (other.gameObject.tag == "Player")
+        if (collision2.gameObject.tag == "Player")
         {
-       
             enemyAnim.SetBool("attacking", false);
         }
     }
-
-
 }
